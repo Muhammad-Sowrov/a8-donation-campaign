e5e6b6799c23a898d23d9f3dcea433d361658d69
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-09-25 16:57:48 +0600

    button color, title color, category color dynamically added

--- src/components/Phones/PhoneCard.jsx
+++ src/components/Phones/PhoneCard.jsx
@@ -1,5 +1,5 @@
 const PhoneCard = ({phone}) => {
-    const {id, picture, title_color, title, category, card_bg_color, category_bg_color } = phone;
+    const {id, picture, title_color, title, category, card_bg_color, category_bg_color, text_button_bg_color } = phone;
   return (
     <div className="px-10 py-5">
       <div className="card w-auto gap-5 container mx-auto " style={{backgroundColor:card_bg_color}}>
@@ -13,7 +13,7 @@ const PhoneCard = ({phone}) => {
         <div className="car-body items-center text-start px-3 py-1">
 
             <div className="card-actions">
-                <button style={{color:category_bg_color}}>{category}</button>
+                <button className="border border-gray-700 rounded p-1 {category_bg_color}" style={{color:category_bg_color, backgroundColor:text_button_bg_color }}>{category}</button>
             </div>
             <p style={{color:title_color}}>{title}</p>
         </div>
